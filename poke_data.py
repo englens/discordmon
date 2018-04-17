cb90410604580079dadf7cce959950223ba65412
Nick Engle <enick11@gmail.com>
2018-04-17 16:57:23 -0400

    included data for all pokemon

--- poke_data.py
+++ poke_data.py
@@ -1,7 +1,7 @@
-import requests
+import requests, json
 
 url = 'http://pokeapi.co/api/v2/'
-
+pokemon_path = './data/pokemon_reference/'
 #grab all a pokemon's data
 def fetch_poke(name):
     return requests.get(url+'pokemon/'+str(name)).json()
@@ -13,3 +13,15 @@ def get_move_list(pokemon):
 def fetch_move(name):
     return requests.get(url+'move/'+str(name)).json()
 
+def write_pokemon(name):
+    txt = fetch_poke(name)
+    print('working on:' + txt['name'])
+    with open(pokemon_path+str(name)+'.txt', 'w+') as f:
+        f.write(json.dumps(txt))
+        
+        
+def write_all_pokemon():
+    for i in xrange(1,151):
+        write_pokemon(i)
+        
+write_pokemon(151)
