c1e8507d7f71cb81cc58ae1fa57cb5fb4003fe14
Nick Engle <enick11@gmail.com>
2018-04-18 13:21:57 -0400

    more data functions

--- poke_data.py
+++ poke_data.py
@@ -6,6 +6,11 @@ pokemon_path = './data/pokemon_reference/'
 def fetch_poke(name):
     return requests.get(url+'pokemon/'+str(name)).json()
 
+def get_poke(id):
+    with open(pokemon_path+str(id)+'.txt') as f:
+        data = json.load(f)
+    return data
+    
 def get_move_list(pokemon):
     return [move['move']['name'] for move in pokemon['moves']]
 
@@ -13,15 +18,24 @@ def get_move_list(pokemon):
 def fetch_move(name):
     return requests.get(url+'move/'+str(name)).json()
 
-def write_pokemon(name):
+def write_pokemon_reference(name):
     txt = fetch_poke(name)
     print('working on:' + txt['name'])
     with open(pokemon_path+str(name)+'.txt', 'w+') as f:
         f.write(json.dumps(txt))
         
         
-def write_all_pokemon():
+def write_all_pokemon_reference():
     for i in xrange(1,151):
         write_pokemon(i)
         
-write_pokemon(151)
+def get_stats(id):
+    poke = get_poke(id)
+    return [stat['base_stat'] for stat in poke['stats']]
+    
+def get_name(id):
+    poke = get_poke(id)
+    return poke['name']
+    
+def fetch_location(id):
+    return requests.get(url+'location/'+str(id)).json()
