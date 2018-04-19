1d5d3417074ce93bb513d739a037395fef91d013
Nick Engle <enick11@gmail.com>
2018-04-19 17:00:30 -0400

    continued work

--- instances.py
+++ instances.py
@@ -1,14 +1,20 @@
+
+ version_id = 1
 class Pokemon:
     """An instance of a type of pokemon. In addition to
     the class stores data on its current attributes, moves, and owner.
     """
-    def __init__(self, id, owner, name=None, level=1):
+    def __init__(self, id, owner, moves, name=None, level=1):
         self.id = id
         self.owner = owner
         self.name = name
         self.level = level
+        self.moves = moves
+    def copy(self):
         
     #return an instance of given pokemon at default stats and level for the given area
-    def make_for_encounter(id, level):
-        #the level of the pokemon is based off the area the pokemon was found in. 
-        return Pokemon(id, 'WILD', level)
+def make_for_encounter(id, location-area):
+    #the level of the pokemon is based off the area the pokemon was found in. 
+    max
+    return Pokemon(id, 'WILD', level)
+    
--- poke_data.py
+++ poke_data.py
@@ -1,4 +1,6 @@
 import requests, json
+"""Various Scripts dealing with the pokeapi database
+and local copies of the data."""
 
 url = 'http://pokeapi.co/api/v2/'
 pokemon_path = './data/pokemon_reference/'
@@ -24,7 +26,6 @@ def write_pokemon_reference(name):
     with open(pokemon_path+str(name)+'.txt', 'w+') as f:
         f.write(json.dumps(txt))
         
-        
 def write_all_pokemon_reference():
     for i in xrange(1,151):
         write_pokemon(i)
@@ -39,3 +40,20 @@ def get_name(id):
     
 def fetch_location(id):
     return requests.get(url+'location/'+str(id)).json()
+    
+def fetch_kanto_locations():
+    kanto = requests.get(url+'region/1/').json()
+    return [requests.get(l['url']).json() for l in kanto['locations']]
+    
+def get_loc_areas(loc):
+    return [requests.get(area['url']).json() for area in loc['areas']]
+    
+def get_area_gen1_pokemon_data(area):
+    names = [a['pokemon']['name'] for a in area['pokemon_encounters']]
+    ids = [a['pokemon']['url'][34:-1] for a in area['pokemon_encounters']]
+    encounter_list = [a['version_details'][0]['encounter_details'] for a in area['pokemon_encounters']]
+    #finish this: it should be a list of tuple of tuples. each list element represents a diferent pokemon,
+    #each element of the outer tuple is a different encounter type, each inner tuple is the (min, max) level for that encounter
+    level_ranges = [(e['max_level'], e['min_level']) for e in encounter_list]
+    return level_ranges
+ranges = get_area_gen1_pokemon_data(get_loc_areas(fetch_kanto_locations()[0])[0])
