9e65b30f07dd86e2ce7cfde49ee5b895e3bcf1a9
Nick Engle <enick11@gmail.com>
2018-10-04 16:11:50 -0400

    removed test file

--- test.py
+++
@@ -1,6 +0,0 @@
-import poke_data
-from pathlib import Path
-all = []
-for move in  [Path(name).stem for name in Path('./data/move_reference/').iterdir()]:
-    for e in poke_data.get_move(move)['effect_entries']:
-        all.append(e['effect'])
